af0da4e0ec3c0bef605688656cc50b6878036db8
Danil Karpenko <81296950+LIMPIX31@users.noreply.github.com>
2024-04-26 17:16:15 +0200

    feat: libraries and jres (#7)

--- crates/spuz_cli/src/main.rs
+++ crates/spuz_cli/src/main.rs
@@ -1,8 +1,5 @@
-use std::{collections::HashSet, ops::Deref};
-
 use anyhow::Result;
 use spuz_folder::Folder;
-use spuz_launcher::Launcher;
 use tokio::fs;
 
 mod telemetry;
@@ -13,37 +10,17 @@ async fn main() -> Result<()> {
 
 	let root = fs::canonicalize("./minecraft").await?;
 	let folder = Folder::settle(&root).await?;
-	let versions = folder.versions();
-	let version = versions.get("1.20.4");
+	// let versions = folder.versions();
+	// let version = versions.get("1.20.4");
+	//
+	// let manifest = version.manifest().await?.expect("No manifest found");
+	//
+	// println!("{manifest:#?}");
 
-	let manifest = version.manifest().await?.expect("No manifest found");
+	let jres = folder.jres();
+	let jre = jres.get("java-runtime-delta");
 
-	let launcher = Launcher::builder()
-		.bin("java".into())
-		.package(manifest.deref().clone().into())
-		.player_name("LIMPIX31".into())
-		.assets_dir(root.join("assets"))
-		.libraries_dir(root.join("libraries"))
-		.natives_dir(version.natives_path.deref().to_owned())
-		.game_dir(root.join("instances").join("test"))
-		.uuid(Some("a5090e59-0ff3-48f8-8135-39785da3e501".into()))
-		.access_token(None)
-		.resolution(None)
-		.xuid(None)
-		.launcher_name(None)
-		.launcher_version(None)
-		.client_id(None)
-		.features(HashSet::new())
-		.client_jar(root.join("versions").join("1.20.4").join("1.20.4.jar"))
-		.build();
-
-	let jvm = launcher.compile();
-
-	let process = jvm.spawn()?;
-
-	while let Some(msg) = process.recv().await {
-		print!("{msg}");
-	}
+	println!("{:?}", jre.bin());
 
 	Ok(())
 }
---
+++ crates/spuz_folder/src/jres.rs
@@ -0,0 +1,57 @@
+use std::{path::Path, sync::Arc};
+use cfg_if::cfg_if;
+
+use tokio::fs::try_exists;
+
+use crate::Result;
+
+#[derive(Debug)]
+pub struct Jres {
+	path: Arc<Path>,
+}
+
+impl Jres {
+	pub(crate) fn from_folder_root(path: &Path) -> Self {
+		let path = path.join("jres").into();
+		Self { path }
+	}
+
+	pub fn get(&self, component: &str) -> JavaRuntime {
+		JavaRuntime::new(self.path.clone(), component)
+	}
+}
+
+type LazyPath = std::cell::OnceCell<Arc<Path>>;
+
+#[derive(Debug)]
+pub struct JavaRuntime {
+	pub component: String,
+	jres_dir: Arc<Path>,
+	bin: LazyPath,
+}
+
+impl JavaRuntime {
+	pub(crate) fn new(jres_dir: Arc<Path>, component: &str) -> Self {
+		Self { jres_dir, component: component.into(), bin: LazyPath::new() }
+	}
+
+	pub fn bin(&self) -> &Arc<Path> {
+		self.bin.get_or_init(|| {
+			let component_dir = self.jres_dir.join(&self.component);
+			
+		  cfg_if! {
+				if #[cfg(target_os = "windows")] {
+					let ext = ".exe";
+				} else {
+					let ext = "";
+				}
+			}
+
+			component_dir.join("bin").join(format!("java{ext}")).into()
+		})
+	}
+
+	pub async fn exists(&self) -> Result<bool> {
+		try_exists(self.bin()).await.map_err(Into::into)
+	}
+}
